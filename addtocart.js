c95075165b590a8438736b6ca115df7ddae5e5e9
wynoahlouis <tabasan.wynoah@gmail.com>
2023-06-16 02:09:27 +0800

    Add to Cart JS - Initial

--- addtocart.js
+++ addtocart.js
@@ -1,13 +1,85 @@
-//DI MUGANA
-function addToCart(item) {
-    var selectedItem = document.createElement('div');
-    selectedItem.classList.add('cartImg');
-    selectedItem.setAttribute('id', addItemId);
-    var img = document.createElement('img');
-    img.setAttribute('src',item.children[0].currentSrc);
-    var title = document.createElement('div');
-    var cartItems = document.getElementById('title');
-    selectedItem.append(img);
-    cartItems.append(selectedItem);
+function addToCart(itemName, itemImage, itemPrice) {
+    var cartItem = document.createElement('div');
+    cartItem.className = 'cart-items';
 
-}
+    var closeButton = document.createElement('span');
+    closeButton.className = 'fas fa-times';
+    closeButton.addEventListener('click', function() {
+      cartItem.remove();
+    });
+    cartItem.appendChild(closeButton);
+
+    var image = document.createElement('img');
+    image.src = itemImage;
+    cartItem.appendChild(image);
+
+    var content = document.createElement('div');
+    content.className = 'content';
+
+    var itemNameElement = document.createElement('h3');
+    itemNameElement.textContent = itemName;
+    content.appendChild(itemNameElement);
+
+    var itemPriceElement = document.createElement('div');
+    itemPriceElement.className = 'price';
+    itemPriceElement.textContent = itemPrice;
+    content.appendChild(itemPriceElement);
+
+    cartItem.appendChild(content);
+
+    
+    var cartContainer = document.querySelector('.cart-items-container');
+    cartContainer.appendChild(cartItem);
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const checkoutBtn = document.querySelector('.checkout_btn');
+    const addtocartBtn = document.querySelector('.addtocartbtn');
+
+
+  
+    
+    checkoutBtn.addEventListener('click', function() {
+      // change ang button pag ma click "Checked Out"
+      checkoutBtn.textContent = 'Checked Out';
+      setTimeout(function() {
+        checkoutBtn.textContent = 'Check Out';
+      }, 1000);
+  
+      // get the cart container
+      const cartContainer = document.querySelector('.cart-items-container');
+  
+      // get all the cart items
+      const cartItems = cartContainer.querySelectorAll('.cart-items');
+
+      // change ang Add to Cart to Added to Cart
+      addtocartBtn.textContent = 'Added to Cart!';
+      setTimeout(function() {
+        checkoutBtn.textContent = 'Check Out';
+      }, 1000);
+  
+  
+      
+      const animationDuration = 500; // milliseconds
+      const delayBetweenItems = 100; // milliseconds
+  
+      // animation function
+      function removeCartItem(item, index) {
+        setTimeout(function() {
+          item.style.transition = `opacity ${animationDuration}ms ease`;
+          item.style.opacity = 0; // fade effect
+          item.addEventListener('transitionend', function() {
+            item.remove(); // mawala ang mga items
+          });
+        }, index * delayBetweenItems);
+      }
+  
+      // animation
+      cartItems.forEach(function(item, index) {
+        removeCartItem(item, index);
+      });
+    });
+  });
+  
+  
+  
